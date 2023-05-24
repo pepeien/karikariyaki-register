3c7d13a36a4d31cda39f0db8932f71c0badbde00
pepeien <erickfcaldeira@gmail.com>
2023-05-24 00:29:10 -0300

    Introduced loading service

---
+++ src/app/services/loading.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+// Automatic
+import { AutomaticAnimation } from '../animations/automatic';
+
+@Injectable({ providedIn: 'root' })
+export class LoadingService {
+	private _loadingSubject: BehaviorSubject<boolean>;
+	private _loadingObersavable: Observable<boolean>;
+
+	constructor() {
+		this._loadingSubject = new BehaviorSubject<boolean>(true);
+		this._loadingObersavable = this._loadingSubject.asObservable();
+	}
+
+	public get loading() {
+		return this._loadingObersavable;
+	}
+
+	public updateLoading(nextLoading: boolean) {
+		this._loadingSubject.next(nextLoading);
+	}
+}
